f969c6c1a70f795f57cc0c5e8e44cbad8bb41b49
Xp1017r <sansoscuridad@gmail.com>
2026-06-16 19:33:21 -0700

    change create command.sh to run the correct create container jobs.

--- .devcontainer/createCommand.sh
+++ .devcontainer/createCommand.sh
@@ -6,7 +6,7 @@ echo "Fixing permissions"
 # get current folder name
 repo=${PWD##*/}
 sudo chown demisto /workspaces /workspaces/$repo
-sudo chown -R demisto /workspaces/$repo/.vscode /workspaces/content/.git /workspaces/$repo/.venv /workspaces/$repo/node_modules /workspaces/$repo/package-lock.json
+sudo chown -R demisto /workspaces/$repo/.vscode /workspaces/$repo/.git /workspaces/$repo/.venv /workspaces/$repo/node_modules /workspaces/$repo/package-lock.json
 
 sudo chown -R demisto $HOME
 
@@ -18,7 +18,6 @@ echo "Setting up content dependencies"
 
 echo "Setting up VSCode"
 poetry run demisto-sdk setup-env
-
 
 echo "Run demisto-sdk pre-commit to cache dependencies"
 poetry run demisto-sdk pre-commit --mode=commit >/dev/null 2>&1 || true
